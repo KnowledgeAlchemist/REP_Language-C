35fc4932562a07364c0d2b300b53b472f4c667cf
KnowalchREP <nowledgalchemis@gmail.com>
2023-11-13 00:17:01 +0000

    Atualizando 13-10 ATIVIDADE PRÁTICA – 2023.2 – MANHÃ

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main()
+{
+  int n, ehPrimo = 1;
+
+  printf("Digite um numero: ");
+  scanf("%d", &n);
+
+  for (int i = 2; i <= n / 2; i++)
+  {
+    if (n % i == 0)
+    {
+      ehPrimo = 0;
+    }
+  };
+
+  if (n <= 1) {
+    ehPrimo = 0;
+  }
+  
+
+    if (ehPrimo == 1) {
+      printf("O numero inserido eh primo");
+    } else {
+      printf("O numero inserido nao eh primo");
+    }
+
   return 0;
 }
